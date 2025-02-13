65f10a6e666ad079b8b5afc882e225e2c09d4929
Faizal2805 <810423243045@dsengg.ac.in>
2025-02-13 10:57:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,5 +38,8 @@ def filter_students():
 
     return jsonify({"department": extracted_department, "year": extracted_year})
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get port from Render, default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)  # Bind to 0.0.0.0
